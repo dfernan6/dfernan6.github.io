b1e9926e52819b15ad79dcfc0deab159cde80b65
unknown <danilo.afernandes@outlook.com>
2025-07-22 02:04:24 -0300

    Finalizando os links provisórios do app inicial

--- app-inicial/src/pages/index.tsx
+++ app-inicial/src/pages/index.tsx
@@ -1,8 +1,18 @@
 export default function Home() {
   return (
-   <div>
+   <div >
     <h1>
-      <a href="https://dfernan6.vercel.app/condicional/faq"> FAQ </a>
+      <a href="https://dfernan6.vercel.app/condicional/faq" > FAQ </a>
+      <a href="https://dfernan6.vercel.app/basicos/caixa">Caixa</a>
+      <a href="https://dfernan6.vercel.app/basicos/circulos">Circulos</a>
+      <a href="https://dfernan6.vercel.app/basicos/eventos">Eventos</a>
+      <a href="https://dfernan6.vercel.app/basicos/primeiro">Primeiro componente</a>
+      <a href="https://dfernan6.vercel.app/hooks/contador">Contador</a>
+      <a href="https://dfernan6.vercel.app/hooks/imagens ">API de imagens</a>
+      <a href="https://dfernan6.vercel.app/layout">Aparência</a>
+      <a href="https://dfernan6.vercel.app/listas/produtos">Produtos</a>
+      <a href="https://dfernan6.vercel.app/listas/basica"></a>
+      <a href="https://dfernan6.vercel.app/starwars"></a>
     </h1>
    </div>
   )
