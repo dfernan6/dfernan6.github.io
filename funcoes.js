a3159d94540f29eb592d68dbd9d387ea3d804dcf
unknown <danilo.afernandes@outlook.com>
2025-08-05 01:51:09 -0300

    Finalizando soft skills

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,12 @@
 function mudaFoto(foto){
     document.getElementById("iIcone").src = foto;
 }
+
+const botoes = document.querySelectorAll(".skills");
+
+botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+    const texto = botao.nextElementSibling;
+    texto.style.display = texto.style.display === "block" ? "none" : "block";
+    });
+});
